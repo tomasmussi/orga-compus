a61a5bbfb3dea09fd7558ec56815d10dd266301b
tomas <tomasmussi@gmail.com>
2016-04-13 16:19:48 -0300

    Agrego un script para correr pruebas

--- tp0/run.sh
+++ tp0/run.sh
@@ -1,6 +1,14 @@
 
 ./tp0 < tests/test$1/__stdin__ > tests/test$1/__out__ 2> tests/test$1/__err__
-diff -iadq tests/test$1/__out__ tests/test$1/__stdout__
-diff -iadq tests/test$1/__err__ tests/test$1/__stderr__
 
-echo $?
+diff -iadq tests/test$1/__out__ tests/test$1/__stdout__
+v1=$?
+diff -iadq tests/test$1/__err__ tests/test$1/__stderr__
+v2=$?
+
+
+if [ $v1 -eq 0 ] && [ $v2 -eq 0 ]; then
+	echo "Paso la prueba $1"
+else
+	echo "NO PASA LA PRUEBA $1!!!!!!!"
+fi
