9600b83e34da6947ebf8a1450fc6c49dc7db5846
tomas <tomasmussi@gmail.com>
2016-04-13 15:47:17 -0300

    Saco funciones que no se usan mas y agrego el test de una matriz de dimension = 500

--- tp0/tp0.c
+++ tp0/tp0.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_SIZE 10
 #define MAX_DIGITOS 1000
 
 /* ------ Implementacion de funciones de matrices ------- */
@@ -123,70 +122,6 @@ void show_help(const char *arg) {
 	}
 }
 
-bool lineaEsValida(char* linea, int indice) {
-	// Copia de la linea original para contar elementos
-	char copiaLinea[indice + 1];
-	strncpy(copiaLinea, linea, (indice + 1));
-	int cantidadElementos = 0;
-	int dimension = 0;
-	char* token = strtok(copiaLinea, " ");
-	dimension = atoi(token);
-	while (token != NULL) {
-		cantidadElementos++;
-		token = strtok(NULL, " ");
-	}
-	if (dimension <= 0)
-		return false;
-	if ( (dimension * dimension * 2) != cantidadElementos - 1 ) {
-		return false;
-	}
-	return true;
-}
-
-void tokenizarLinea(char* linea, int indice) {
-	if (!lineaEsValida(linea, indice)) {
-		fprintf(stderr, "La dimension no concuerda con la cantidad de elementos de la linea.\n");
-		return;
-	} 
-	int cantidadElementos = 0;
-	int dimension = 0;
-	char* token = strtok(linea, " ");
-	dimension = atoi(token);
-	int cantidadElementosM1 = dimension * dimension;
-	double elementosM1[cantidadElementosM1];
-	double elementosM2[cantidadElementosM1];
-	int indice1 = 0;
-	int indice2 = 0;
-	while (token != NULL) {
-		cantidadElementos++;
-		if (cantidadElementos == 1) {
-			// Es la dimension
-			token = strtok(NULL, " ");
-			continue;
-		}
-
-		if (cantidadElementos - 1 <= cantidadElementosM1) {
-			elementosM1[indice1] = atof(token);
-			indice1++;
-		} else {
-			elementosM2[indice2] = atof(token);
-			indice2++;
-		}
-		token = strtok(NULL, " ");
-	}
-
-	/* Se deberian cargar las matrices */
-	matrix_t* matrizA = create_matrix(dimension, dimension);
-	matrix_t* matrizB = create_matrix(dimension, dimension);
-	load_matrix(matrizA, elementosM1);
-	load_matrix(matrizB, elementosM2);
-	matrix_t *producto = matrix_multiply(matrizA, matrizB);
-	print_matrix(stdout, producto);
-	destroy_matrix(matrizA);
-	destroy_matrix(matrizB);
-	destroy_matrix(producto);
-}
-
 void multiplicar(int dimension, double *matriz1, double *matriz2) {
 	matrix_t* matrizA = create_matrix(dimension, dimension);
 	matrix_t* matrizB = create_matrix(dimension, dimension);
@@ -267,47 +202,6 @@ void leerLinea(FILE* archivo, int* cantidadLineas) {
 	free(matriz1);
 	free(matriz2);
 }
-/*
-void leerLinea(FILE* archivo, int* cantidadLineas) {
-	unsigned long long tamanioLinea = MAX_SIZE;
-	char* bufferLinea = (char*) malloc(sizeof(char) * tamanioLinea);
-	if (bufferLinea == NULL) {
-		printf("No se puede asignar memoria al bufferLinea \n");
-		exit(1);
-	}
-	char caracter = getc(archivo);
-	int indice = 0;
-	while ((caracter != '\n') && (caracter != EOF)) {
-		if (indice >= MAX_SIZE) {
-			tamanioLinea += MAX_SIZE;
-			if (tamanioLinea >= 2147480000) {
-				printf("Tamanio mayor a 2 mil millones\n");
-			}
-			char *nuevoBufferLinea = realloc(bufferLinea, tamanioLinea);
-			if (bufferLinea == NULL) {
-				free(bufferLinea);
-				printf("No se puede reasignar memoria al bufferLinea \n");
-				exit(1);
-			}
-			bufferLinea = nuevoBufferLinea;
-		}
-		bufferLinea[indice] = caracter;
-		indice++;
-		caracter = getc(archivo);
-	}
-	// Termina el bufferLinea
-	bufferLinea[indice] = '\0';
-
-	char resultado[indice + 1];
-	strncpy(resultado, bufferLinea, (indice + 1));
-	free(bufferLinea);
-
-	// Si la linea no esta vacia
-	if (strlen(resultado) > 0) {
-		*cantidadLineas += 1;
-		tokenizarLinea(resultado, indice + 1);
-	}
-}*/
 
 int main(int argc, char *argv[]) {
 	if (argc != 1) {
