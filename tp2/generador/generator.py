bd8abfd0e77da0482d4a7057a326b8657a3a6855
tomas <tomasmussi@gmail.com>
2016-06-02 14:59:56 -0300

    Agrego un generador de matrices en python. Los cache, tanto in como out que son referencias de input y output esperado. Agrego tambien las ejecuciones que hice con valgrind cache profiler y que ya escribi en el informe

---
+++ tp2/generador/generator.py
@@ -0,0 +1,31 @@
+
+
+
+def generarMatriz(tam, elemento = "1"):
+	matriz = ""
+	for i in xrange(tam):
+		for j in xrange(tam):			
+			matriz += elemento
+			if ((i != tam -1) or (j != tam -1)):
+				matriz += " "
+	return matriz
+	
+def generarInput(tam):
+	linea = str(tam) + " "
+	linea += generarMatriz(tam)
+	linea += " "
+	linea += generarMatriz(tam)
+	return linea
+	
+def generarResultado(tam):
+	linea = str(tam) + " "
+	linea += generarMatriz(tam, str(tam))
+	return linea
+	
+def main():
+	m = generarResultado(256)
+	print m
+
+	
+if __name__ == "__main__":
+	main();
